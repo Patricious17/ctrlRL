02c12a56a9c4934fc35fdcad4aac6301a47fc067
patricious17 <patrikkolaric92@gmail.com>
2020-03-14 21:27:41 -0500

    dot product 1

--- main.py
+++ main.py
@@ -12,17 +12,16 @@ def newtonKleinman(A,B,Q,K, N):
     assert ((Eopt < 0).min() or ((la.eig(Popt)[0]) > 0).min()), 'There is no stabilizing solution to Riccati equation for provided (A,B,Q,R), your efforts are futile!'
 
     # P that corresponds to initial gain K
-    P = mtl.lyap((A-B*K).transpose(), (Q + K.transpose() * K)); assert ((la.eig(P)[0])>0).min(), 'P iterate is not positive definite!'
+    P = mtl.lyap((A-np.dot(B, K)).transpose(), (Q + np.dot(K.transpose(), K))); assert ((la.eig(P)[0])>0).min(), 'P iterate is not positive definite!'
+    errorP.append(la.norm(P - Popt))
 
     for i in range(N):
         '''Newton-Kleinman is repeated Lyapunov equation that yields iterates P. Each P that constitutes Lyapunov function x'Px '''
-        P = mtl.lyap((A - B * (B.transpose() * P)).transpose(),
-                     (Q + (B.transpose() * P).transpose() * (B.transpose() * P))); assert ((la.eig(P)[0])>0).min(), 'P iterate is not positive definite!'
+        P = mtl.lyap((A - np.dot(B, np.dot(B.transpose(), P))).transpose(),
+                     (Q + np.dot(np.dot(B.transpose(), P).transpose(), np.dot(B.transpose(), P)))); assert ((la.eig(P)[0])>0).min(), 'P iterate is not positive definite!'
         errorP.append(la.norm(P-Popt))
 
-    plt.plot(errorP, 'o')
-    plt.grid()
-    plt.show()
+    plt.plot(errorP, 'o');    plt.grid();    plt.show()
     pass
 
 def inverseReinfLearning_Lyap1(A, B, Qtrue, N):
@@ -77,17 +76,13 @@ if __name__== "__main__":
     eigs = la.eig(A);    eval = eigs[0];    evec = eigs[1];    print('eigen values of A:\n', eval)
     B = np.array([[2], [1.6]]);     B1 = np.array([[2], [1.6]])
     Q = np.array([[6, 0], [0, 6]])
-    K0,X,E = mtl.lqr(A,B, 19*Q,1)
+    q=np.random.rand(2, 2)
+    K0,X,E = mtl.lqr(A,B, 0.2*np.dot(q.transpose(), q),1)
 
-    eigs = la.eig(A-B*K0);    eval = eigs[0];    evec = eigs[1];    print('eigen values of Acl:\n', eval)
+    eigs = la.eig(A-np.dot(B,K0));    eval = eigs[0];    evec = eigs[1];    print('eigen values of Acl:\n', eval)
     #P = mtl.lyap((A-B*K0).transpose(),Q);    eigs = la.eig(P);    eval = eigs[0];    evec = eigs[1];    print('eigen values of P:\n', eval)
 
-    #newtonKleinman(A, B, Q, K0, 10)
-    inverseReinfLearning_Lyap1(A,B,Q,50)
+    newtonKleinman(A, B, Q, K0, 10)
+    #inverseReinfLearning_Lyap1(A,B,Q,50)
 
     print('Main finished')
-
-
-
-
-
