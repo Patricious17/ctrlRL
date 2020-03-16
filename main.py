dbbc4136b56bd656ef804bcf9e5568aad00fd63a
Patrik <Patrikkolaric92@gmail.com>
2020-03-16 10:18:26 -0700

    Added 2nd attempt

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ import matplotlib.pyplot as plt
 
 def newtonKleinman(A,B,Q,K, N):
     assert ((la.eig(A-B*K)[0]) < 0).min(), 'initial gain K is not stabilizing, Newton-Kleinman is not guaranteed to converge!'
-    errorP = []
     Kopt, Popt, Eopt = mtl.lqr(A, B, Q, 1)
     assert ((Eopt < 0).min() or ((la.eig(Popt)[0]) > 0).min()), 'There is no stabilizing solution to Riccati equation for provided (A,B,Q,R), your efforts are futile!'
 
     # P that corresponds to initial gain K
     P = mtl.lyap((A-np.dot(B, K)).transpose(), (Q + np.dot(K.transpose(), K))); assert ((la.eig(P)[0])>0).min(), 'P iterate is not positive definite!'
+    errorP = []
     errorP.append(la.norm(P - Popt))
 
     for i in range(N):
@@ -23,6 +23,37 @@ def newtonKleinman(A,B,Q,K, N):
 
     plt.plot(errorP, 'o');    plt.grid();    plt.show()
     pass
+
+def inverseReinfLearning1(A,B,Qtrue,N):
+    Popt, Eopt, Kopt = mtl.care(A, B, Qtrue, 1);    Aopt = A - B * Kopt
+    Q = Qtrue*1.1;    P, E, K = mtl.care(A, B, Q, 1)
+    errorP = [];    errorP.append(la.norm(P - Popt))
+
+    for i in range(5):
+        print(i)
+        Qlyap = 2*np.dot(np.dot(B.transpose(), P).transpose(), np.dot(B.transpose(), P)) - np.dot(A.transpose(), P) - np.dot(P,A)
+        Qlyap = 0.5*(Qlyap + Qlyap.transpose()); assert ((la.eig(Qlyap)[0])>0).min(), 'Qlyap iterate is not positive definite!'
+        P = mtl.lyap(Aopt.transpose(), Qlyap); assert ((la.eig(P)[0])>0).min(), 'P iterate is not positive definite!'
+        errorP.append(la.norm(P - Popt))
+
+    plt.plot(errorP, 'o');    plt.grid();    plt.show()
+    pass
+
+def inverseReinfLearning2(A,B,Qtrue,N):
+    Popt, Eopt, Kopt = mtl.care(A, B, Qtrue, 1);    Aopt = A - B * Kopt
+    Q = Qtrue*1.1;    P, E, K = mtl.care(A, B, Q, 1)
+    errorP = [];    errorP.append(la.norm(P - Popt))
+
+    for i in range(5):
+        Qlyap = -2*np.dot(np.dot(B.transpose(), P).transpose(), np.dot(B.transpose(), P)) - np.dot(A.transpose(), P) - np.dot(P,A)
+        Qlyap = 0.5*(Qlyap + Qlyap.transpose()); assert ((la.eig(Qlyap)[0])>0).min(), 'Qlyap iterate is not positive definite!'
+        P = mtl.lyap(A.transpose(), Qlyap); assert ((la.eig(P)[0])>0).min(), 'P iterate is not positive definite!'
+        errorP.append(la.norm(P - Popt))
+
+    plt.plot(errorP, 'o');    plt.grid();    plt.show()
+    pass
+
+
 
 def inverseReinfLearning_Lyap1(A, B, Qtrue, N):
     errorP = []
@@ -73,16 +104,19 @@ def inverseReinfLearning_Lyap2(A, B, Qtrue, N):
 if __name__== "__main__":
     print('Main executed')
     A = np.array([[-1,  2],[2.2,  1.7]]);     A1 = np.array([[-1,  2],[2.2,  1.7]])
-    eigs = la.eig(A);    eval = eigs[0];    evec = eigs[1];    print('eigen values of A:\n', eval)
+    #eigs = la.eig(A);    eval = eigs[0];    evec = eigs[1];    print('eigen values of A:\n', eval)
     B = np.array([[2], [1.6]]);     B1 = np.array([[2], [1.6]])
     Q = np.array([[6, 0], [0, 6]])
-    q=np.random.rand(2, 2)
-    K0,X,E = mtl.lqr(A,B, 0.2*np.dot(q.transpose(), q),1)
+    q=np.random.rand(2, 2);    Qrand = np.dot(q.transpose(), q)
 
-    eigs = la.eig(A-np.dot(B,K0));    eval = eigs[0];    evec = eigs[1];    print('eigen values of Acl:\n', eval)
-    #P = mtl.lyap((A-B*K0).transpose(),Q);    eigs = la.eig(P);    eval = eigs[0];    evec = eigs[1];    print('eigen values of P:\n', eval)
+    #K0,X,E = mtl.lqr(A,B, Qrand,1)
+    K0, X, E = mtl.lqr(A, B, Q, 1)
 
-    newtonKleinman(A, B, Q, K0, 10)
+    #eigs = la.eig(A-np.dot(B,K0));    eval = eigs[0];    evec = eigs[1];    print('eigen values of Acl:\n', eval)
+
+    #newtonKleinman(A, B, Q, K0, 10)
+    inverseReinfLearning1(A, B, Q, 10)
+    inverseReinfLearning2(A, B, Q, 4)
     #inverseReinfLearning_Lyap1(A,B,Q,50)
 
     print('Main finished')
